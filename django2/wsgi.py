0be80c6b2542bdcecb196b19f155f95c85fcb3a0
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-11-12 22:27:16 -0300

    import dj-static

--- django2/wsgi.py
+++ django2/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django2.settings')
 
-#application = Cling(MediaCling(get_wsgi_application()))
+application = Cling(MediaCling(get_wsgi_application()))
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
