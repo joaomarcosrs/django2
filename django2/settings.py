a0124badb3e10f9bae7367792afb4e4ad046128b
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-11-12 23:01:07 -0300

    import dj-static

--- django2/settings.py
+++ django2/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEIDA_URL = '/core/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEIDA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'django2/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
