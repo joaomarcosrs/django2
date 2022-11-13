ff814f528049f66fba9d9dd0bece22c0b26ad12c
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-11-12 22:54:47 -0300

    import dj-static

--- django2/settings.py
+++ django2/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEIDA_URL = '/media/'
+MEIDA_URL = 'django2/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
