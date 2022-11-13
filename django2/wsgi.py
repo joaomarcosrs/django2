6ff6195cbca3e9a2dd666cb283fe5c85227d6c48
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-11-12 22:24:05 -0300

    Teste arquivo de midia

--- django2/wsgi.py
+++ django2/wsgi.py
@@ -14,4 +14,6 @@ from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django2.settings')
 
-application = Cling(MediaCling(get_wsgi_application()))
+#application = Cling(MediaCling(get_wsgi_application()))
+
+application = get_wsgi_application()
