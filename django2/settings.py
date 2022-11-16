a21bf17579b2be26ba05340d8ad8fb09cdd3e814
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-11-16 18:56:28 -0300

    Melhoria Media files

--- django2/settings.py
+++ django2/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://django2-jmrs.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
