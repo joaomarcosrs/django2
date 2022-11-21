4a1c2c5c2c58f7abbfdb6c067701634f2a0067d3
joaomarcosrs <joaomarcosrs40@gmail.com>
2022-11-21 17:25:20 -0300

    Organizando arquivos estáticos

--- django2/storage_backends.py
+++ django2/storage_backends.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S3Boto3Storage):
+""" class StaticStorage(S3Boto3Storage):
     location = 'static'
-    default_acl = 'public-read'
+    default_acl = 'public-read' """
 
 
 class PublicMediaStorage(S3Boto3Storage):
